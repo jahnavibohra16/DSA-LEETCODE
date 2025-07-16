f4bcb87d9e443ebf775d44b2f2b2b253d1c92b3b
Jahnavi Bohra <116445914+jahnavibohra16@users.noreply.github.com>
2025-07-17 02:09:02 +0530

    Time: 2 ms (68.68%), Space: 23.3 MB (74.77%) - LeetHub

--- 0155-min-stack/0155-min-stack.cpp
+++ 0155-min-stack/0155-min-stack.cpp
@@ -1,31 +1,47 @@
 class MinStack {
 public:
-    stack <pair<int, int>> st;
+    stack<long long> st;
+    long long mini;
+    long long x;
+
     MinStack() {
-        
+        // nothing to initialize here
     }
     
     void push(int val) {
-        if (st.empty()){
-            st.push({val,val});
-        }
-        else{
-            st.push({val, min(val,st.top().second)});
+        if (st.empty()) {
+            mini = val;
+            st.push(val);
+        } else {
+            if (val > mini) {
+                st.push(val);
+            } else {
+                st.push(2LL * val - mini);  // encode using long long
+                mini = val;
+            }
         }
     }
     
     void pop() {
+        if (st.empty()) return;
+        x = st.top();
         st.pop();
+        if (x < mini) {
+            mini = 2 * mini - x;  // decode the previous mini
+        }
     }
     
     int top() {
-        return st.top().first;
+        if (st.empty()) return -1;
+        x = st.top();
+        return (x < mini) ? mini : x;
     }
     
     int getMin() {
-        return st.top().second;
+        return mini;
     }
 };
+
 
 /**
  * Your MinStack object will be instantiated and called as such:
