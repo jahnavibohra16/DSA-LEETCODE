58bc489114e3eca11edcf83a710f3602408a86ea
Jahnavi Bohra <116445914+jahnavibohra16@users.noreply.github.com>
2026-06-22 23:19:36 +0530

    Time: 3 ms (68.67%), Space: 10.8 MB (92.89%) - LeetHub

---
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        int maxlen = 0, l = 0, r = 0, maxf = 0;
+        vector<int> hash(26,0);
+        int n = s.size();
+        while(r<n){
+            hash[s[r]-'A']++;
+            maxf = max(maxf, hash[s[r]-'A']);
+            if((r-l+1)-maxf > k){
+                hash[s[l]-'A']--;  
+                l++;
+            }
+            if((r-l+1)-maxf <=k){
+                maxlen = max(maxlen, r-l+1);
+            }
+            r++;
+        }
+        return maxlen;
+    }
+};
